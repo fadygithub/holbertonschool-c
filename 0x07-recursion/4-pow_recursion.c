58603b60788081e28f464c9ed0d47e5bf9472fab
Fady Hanna <fadyhanna1@outlook.com>
2021-08-16 13:30:29 +0300

    Debug task 4

--- 0x07-recursion/4-pow_recursion.c
+++ 0x07-recursion/4-pow_recursion.c
@@ -1,23 +1,17 @@
 #include "holberton.h"
-#include <stdio.h>
-
 /**
-*  _pow_recursion - calculates value of x raised to the power of y
-* @x: given number
-* @y: exponent
-* Return: value of x raised to the power of y
-**/
+ * _pow_recursion - calculate power of the number.
+ *x: integer value.
+ *y: integer value.
+ *
+ *Return: the value of x raised to the power of y..
+ */
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
-	{
 		return (-1);
-	}
 	if (y == 0)
-	{
 		return (1);
-	}
-	printf("%d\n", x * x);
-	return (x * _pow_recursion(x, y - 1));
+	else
+		return (x * _pow_recursion(x, y - 1));
 }
-
