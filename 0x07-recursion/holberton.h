cb6e4f2885fb1c3d3677cf77ffe7582807969b94
Fady Hanna <fadyhanna1@outlook.com>
2021-08-16 13:28:47 +0300

    Debug taskk 100

--- 0x07-recursion/holberton.h
+++ 0x07-recursion/holberton.h
@@ -1,7 +1,4 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
-int _putchar(char c);
+int _putchar(char);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -11,5 +8,4 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
-
-#endif /* HOLBERTON_H */ 
+char *wildcard(char *s3, char *s4);
